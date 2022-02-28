48f15511ae40c1ad7cb6cad5db21342ff2fc0f23
charlypradel <charlotte.pradel@gmail.com>
2022-02-28 21:30:27 +0100

    initial commit

---
+++ my-app/src/components/neverever.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class NeverEver extends Component {
+    render() {
+        return (
+            <div>NeverEver</div>
+        )
+    }
+}
+
+export default NeverEver;
