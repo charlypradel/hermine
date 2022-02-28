54b4799fbd3f5d7922d4395fc121b970a485fdc2
charlypradel <charlotte.pradel@gmail.com>
2022-02-28 21:29:53 +0100

    initial commit

---
+++ my-app/src/components/ringOfFire.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class RingOfFire extends Component {
+    render() {
+        return (
+            <div>RingOfFire</div>
+        )
+    }
+}
+
+export default RingOfFire;
