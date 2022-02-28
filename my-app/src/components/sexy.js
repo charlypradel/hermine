4f954b6977604756b1607910e62618bb4f109830
charlypradel <charlotte.pradel@gmail.com>
2022-02-28 21:39:01 +0100

    running - missing http

---
+++ my-app/src/components/sexy.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import Header from './header'
+import Footer from './footer'
+import GameCards from './gameCard'
+
+export class Sexy extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            fetched_data: false,
+            tasks: ["Test1", "Test2", "Test3", "Test4"],
+            current_task: "TEST",
+        }
+        this.getNextTask = this.getNextTask.bind(this)
+    }
+
+    getNextTask() {
+        var allTasks = this.state.tasks
+        var numTasks = allTasks.length;
+        console.log("Num Tasks: ", numTasks)
+        if (!numTasks) {
+            let end_task = <Link to="/" style={{ textDecoration: 'none', color: 'white', fontSize: 'auto' }}><b>You naughty thing! &#128520; &#127866; &#127881;<br />One could almost think that we're related &#128521; </b></Link>
+            this.setState((state, props) => ({
+                current_task: end_task,
+            }))
+        }
+        else {
+            var index = Math.floor(Math.random() * numTasks)
+            var removed = allTasks.splice(index, 1)
+            this.setState((state, props) => ({
+                current_task: removed[0],
+                tasks: allTasks,
+            }))
+        }
+
+    }
+
+    render() {
+        return (
+            <div className='App'>
+                <Header />
+                <button className='cardholder' onClick={this.getNextTask}>
+                    <GameCards task={this.state.current_task} />
+                </button>
+                <Footer />
+            </div>
+        )
+    }
+}
+
+export default Sexy
