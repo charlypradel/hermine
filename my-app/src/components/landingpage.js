fb0854761b2b2b6590b2c5e2ae44ea54344d2d18
charlypradel <charlotte.pradel@gmail.com>
2022-02-28 21:31:25 +0100

    running

--- my-app/src/components/landingpage.js
+++ my-app/src/components/landingpage.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 
 export class Landingpage extends Component {
+
     render() {
         return (
-            <div className='start_text'>
-                Hello!<br />So nice of you to pay us a visit.
-                Granny will take good care of you and your friends.<br />
-                In which mood are you today?
+            <div className='welcome_page'>
+                <div className='start_text'>
+                    <b>Hello my dear!<br />So nice of you to pay us a visit.<br />
+                        Granny will take good care of you and your friends.<br /><br />
+                        In which mood are you today?</b>
+                </div>
+                <div className='select_category'>
+                    <Link to={{ pathname: "/mixItUp" }} className='category_item'> Mix it up </Link>
+                    <Link to={{ pathname: "/neverever" }} className='category_item'> Granny never did ... &#128124; </Link>
+                    <Link to={{ pathname: "/todos" }} className='category_item'> Get your hands dirty &#128521;</Link>
+                    <Link to={{ pathname: "/sexy" }} className='category_item'> Don't be a softie &#128520; </Link>
+                    <Link to={{ pathname: "/ringOfFire" }} className='category_item'> Ring of Fire &#128293; </Link>
+                    <Link to={{ pathname: "/contribute" }} className='category_item' style={{ backgroundColor: '#FE6A16' }}> Contribute a task/question </Link>
+                </div>
             </div>
+
         )
     }
-} 
+}
 export default Landingpage;
