a92aeaf68f824e71aeb261798c044661083e0213
charlypradel <charlotte.pradel@gmail.com>
2022-02-28 21:30:00 +0100

    initial commit

---
+++ my-app/src/components/todos.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class TODOs extends Component {
+    render() {
+        return (
+            <div>TODOs</div>
+        )
+    }
+}
+
+export default TODOs;
