6239d729218fd1353984ec7735511ec368a5f9c5
charlypradel <charlotte.pradel@gmail.com>
2022-02-28 21:31:35 +0100

    running

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,18 @@
 import './App.css';
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header';
+import Footer from './components/footer';
 import Landingpage from './components/landingpage';
-import MixItUp from './components/mixItUp';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      category: "",
-    }
-  }
+
   render() {
     return (
-      <Router>
-        <div className="App" >
-          <Header />
-          <Routes>
-            <Route exact path='/' component={Landingpage} />
-            <Route exact path='/mixItUp' component={MixItUp} category={this.state.category} />
-          </Routes>
-
-        </div>
-
-      </Router>
-
+      <div className="App">
+        <Header />
+        <Landingpage />
+        <Footer />
+      </div>
     )
   }
 }
